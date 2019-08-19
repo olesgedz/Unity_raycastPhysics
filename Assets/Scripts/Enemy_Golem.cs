2f8e73ce0c2388ba35221915582654b26163ae95
olesgedz <gedzoles@gmail.com>
2019-08-19 19:20:19 +0300

    19.08.19 - bug fix

--- Assets/Scripts/Enemy_Golem.cs
+++ Assets/Scripts/Enemy_Golem.cs
@@ -3,42 +3,42 @@ using System.Collections;
 
 public class Enemy_Golem : MonoBehaviour
 {
-    public LayerMask enemyMask;
-    public float speed = 5;
-    Rigidbody2D myBody;
-    Transform myTrans;
-    float myWidth, myHeight;
+    public LayerMask LayerMask;
+    public float golemSpeed = 5;
+    Rigidbody2D golemBody;
+    Transform golemTrans;
+    float golemWidth, golemHeight;
 
     void Start()
     {
-        myTrans = this.transform;
-        myBody = this.GetComponent<Rigidbody2D>();
+        golemTrans = this.transform;
+        golemBody = this.GetComponent<Rigidbody2D>();
         SpriteRenderer mySprite = this.GetComponent<SpriteRenderer>();
-        myWidth = mySprite.bounds.extents.x;
-        myHeight = mySprite.bounds.extents.y;
+        golemWidth = mySprite.bounds.extents.x;
+        golemHeight = mySprite.bounds.extents.y;
     }
 
     void Update()
     {
-        Vector2 myVel = myBody.velocity;
-        Vector2 lineCastPos = myTrans.position.toVector2() - myTrans.right.toVector2() * myWidth - Vector2.up * myHeight;
+        Vector2 myVel = golemBody.velocity;
+        Vector2 lineCastPos = golemTrans.position.toVector2() - golemTrans.right.toVector2() * golemWidth - Vector2.up * golemHeight;
         Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
-        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, LayerMask);
 
-        Debug.DrawLine(lineCastPos, lineCastPos - myTrans.right.toVector2() * .05f);
-        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - myTrans.right.toVector2() * .05f, enemyMask);
+        Debug.DrawLine(lineCastPos, lineCastPos - golemTrans.right.toVector2() * .05f);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - golemTrans.right.toVector2() * .05f, LayerMask);
 
         if (!isGrounded || isBlocked)
         {
            if (myVel.y >= 0.0f)
             {
-                Vector3 currRot = myTrans.eulerAngles;
+                Vector3 currRot = golemTrans.eulerAngles;
                 currRot.y += 180;
-                myTrans.eulerAngles = currRot;
+                golemTrans.eulerAngles = currRot;
            }
         }
        
-        myVel.x = -myTrans.right.x * speed;
-        myBody.velocity = myVel;
+        myVel.x = -golemTrans.right.x * golemSpeed;
+        golemBody.velocity = myVel;
     }
 }
